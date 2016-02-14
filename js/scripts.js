a5211caf04ec5e647e9c1929682d457ace6fbcae
Selina Cleary <selinacleary@Selinas-MacBook-Pro.local>
2016-02-14 12:21:04 -0800

    added waypoints, refactored and fixed page scrolling bugs

--- js/scripts.js
+++ js/scripts.js
@@ -19,20 +19,14 @@ $(function () {
         //button fades out 
         $('.button').fadeOut(1000);
 
+        //spotlight fades in and out
+        $('.lightbeam').delay(1000).fadeIn(1500).fadeOut(2000);
+
+        //and then bowie fades in
+        $('.naked').delay(2000).fadeIn(1000);
+
     });
 });
-
-
-$(function () {
-    //spotlight fades in and out
-    $('.lightbeam').delay(2000).fadeIn(1500).fadeOut(2000);
-
-    //and then bowie fades in
-    $('.naked').delay(2000).fadeIn(1000);
-});
-
-
-
 
 //*REPETITIVE EVENTS*//
 
@@ -57,7 +51,7 @@ $(function () {
 $(function () {
 
     //when user clicks on first link
-    $('.intropage_link').click(function () {
+    $('.intro_page_link').click(function () {
         //page scrollTo top
         $(window).scrollTo($('header'), 1000);
         //curtains slide in
@@ -72,44 +66,33 @@ $(function () {
     //when user clicks a link in the nav
     $('.page1_link').click(function () {
         //scroll to that particular page, smoothly
-        $(window).scrollTo($('.page1'), 1000);
-
+        $(window).scrollTo($('.section1'), 1000);
     });
-
     $('.page2_link').click(function () {
         $(window).scrollTo($('.section2'), 1000);
-
     });
 
-    $('.page4_link').click(function () {
+    $('.page3_link').click(function () {
         $(window).scrollTo($('.section3'), 1000);
     });
 
-    $('.page5_link').click(function () {
+    $('.page4_link').click(function () {
         $(window).scrollTo($('.section4'), 1000);
     });
 
-
-    $('.page6_link').click(function () {
+    $('.page5_link').click(function () {
         $(window).scrollTo($('.section5'), 1000);
     });
 
-    $('.page7_link').click(function () {
+    $('.page6_link').click(function () {
         $(window).scrollTo($('.section6'), 1000);
     });
-
-
-    $('.page8_link').click(function () {
+    $('.page7_link').click(function () {
         $(window).scrollTo($('.section7'), 1000);
     });
-
-
-    $('.page9_link').click(function () {
+    $('.page8_link').click(function () {
         $(window).scrollTo($('.section8'), 1000);
     });
-
-
-
 
 });
 //*waypoints*//
@@ -117,11 +100,100 @@ $(function () {
 
 
 $(function () {
-
+    $('.section1').waypoint(function (direction) {
+        $('nav a div').removeClass('selected', 200);
+        if (direction === 'down') {
+            $('.circle1').addClass('selected', 200);
+            $('.fadeIn').fadeIn();
+        } else {
+            $('.circle0').addClass('selected', 200);
+        }
+    });
 });
 
+$(function () {
+    $('.section2').waypoint(function (direction) {
+        $('nav a div').removeClass('selected', 200);
+        if (direction === 'down') {
+            $('.circle2').addClass('selected', 200);
+            $('.fadeIn').fadeIn();
+        } else {
+            $('.circle1').addClass('selected', 200);
+        }
+    });
+});
 
+$(function () {
+    $('.section3').waypoint(function (direction) {
+        $('nav a div').removeClass('selected', 200);
+        if (direction === 'down') {
+            $('.circle3').addClass('selected', 200);
+            $('.fadeIn').fadeIn();
+        } else {
+            $('.circle2').addClass('selected', 200);
+        }
+    });
+});
 
+$(function () {
+    $('.section4').waypoint(function (direction) {
+        $('nav a div').removeClass('selected', 200);
+        if (direction === 'down') {
+            $('.circle4').addClass('selected', 200);
+            $('.fadeIn').fadeIn();
+        } else {
+            $('.circle3').addClass('selected', 200);
+        }
+    });
+});
+
+$(function () {
+    $('.section5').waypoint(function (direction) {
+        $('nav a div').removeClass('selected', 200);
+        if (direction === 'down') {
+            $('.circle5').addClass('selected', 200);
+            $('.fadeIn').fadeIn();
+        } else {
+            $('.circle4').addClass('selected', 200);
+        }
+    });
+});
+
+$(function () {
+    $('.section6').waypoint(function (direction) {
+        $('nav a div').removeClass('selected', 200);
+        if (direction === 'down') {
+            $('.circle6').addClass('selected', 200);
+            $('.fadeIn').fadeIn();
+        } else {
+            $('.circle5').addClass('selected', 200);
+        }
+    });
+});
+
+$(function () {
+    $('.section7').waypoint(function (direction) {
+        $('nav a div').removeClass('selected', 200);
+        if (direction === 'down') {
+            $('.circle7').addClass('selected', 200);
+            $('.fadeIn').fadeIn();
+        } else {
+            $('.circle6').addClass('selected', 200);
+        }
+    });
+});
+
+$(function () {
+    $('.section8').waypoint(function (direction) {
+        $('nav a div').removeClass('selected', 200);
+        if (direction === 'down') {
+            $('.circle8').addClass('selected', 200);
+            $('.fadeIn').fadeIn();
+        } else {
+            $('.circle7').addClass('selected', 200);
+        }
+    });
+});
 
 
 
